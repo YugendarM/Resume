84a0a58340324631792f517ccbc0052e592681af
YugendarM <yugendar2411@gmail.com>
2023-02-14 17:15:11 +0530

    Changes have been made

--- script.js
+++ script.js
@@ -13,3 +13,9 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelector('.hidden')
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+function greet(username) {
+    var username= document.getElementById("name").value;
+    alert("Thanks "+ username +" for submitting");
+}
